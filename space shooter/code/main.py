89f5e88b81669b0cb32d94d90b2b95495b4d7cea
notJunius <120129256+notJunius@users.noreply.github.com>
2025-09-29 17:27:10 -0600

    day 2, moved a meteor

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -8,21 +8,28 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 running = True
+clock = pygame.time.Clock()
 
+#player stuff
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_pos = pygame.Vector2(50, 100)
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.Vector2(1, 1)
 
+#star stuff
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 stars = []
 
+#meteor stuff
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+meteor_direction = pygame.Vector2(1, -1)
+meteor_speed = 20
 
+
+#laser stuff
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
-player_speed = 0.2
 for i in range(20):
     x = randint(0, WINDOW_WIDTH)
     y = randint(0, WINDOW_HEIGHT)
@@ -30,16 +37,20 @@ for i in range(20):
 
 while running:
     #event loop
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     #i'm gonna put game logic here
-    player_rect.centerx += player_speed
-    if player_rect.right >= WINDOW_WIDTH:
-        player_speed *= -1
-    elif player_rect.left <= 0:
-        player_speed *= -1
+    player_rect.center += player_direction
+
+    #meteor movement logic
+    meteor_rect.center += meteor_direction * meteor_speed
+    if meteor_rect.right >= WINDOW_WIDTH or meteor_rect.left <= 0:
+        meteor_direction.x *= -1
+    elif meteor_rect.top <= 0 or meteor_rect.bottom >= WINDOW_HEIGHT:
+        meteor_direction.y *= -1
 
 
     #draw the game
@@ -56,4 +67,4 @@ while running:
 
 pygame.quit()
 
-# left off at 1 hr 4 min 18 seconds
+# left off at 1 hr 14 min 59 seconds
