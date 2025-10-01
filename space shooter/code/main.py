efdb6478c99d645e0700f7c0af36f0046ab77a0c
notJunius <120129256+notJunius@users.noreply.github.com>
2025-10-01 08:28:34 -0600

    made objects and learned about timers

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -1,7 +1,49 @@
 from os.path import join
 from random import randint
 import pygame
-from pygame.mixer_music import play
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups) -> None:
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2()
+        self.speed = 500
+
+        #cooldown
+        self.can_shoot = True
+        self.laser_shoot_time = 0
+        self.cooldown_duration = 2000
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
+
+
+
+    def update(self, dt) -> None:
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+        if pygame.key.get_just_pressed()[pygame.K_SPACE] and self.can_shoot:
+            print("fire laser")
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+        
+        self.laser_timer()
+
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, window_width, window_height, image) -> None:
+        super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_frect(center = (randint(0, window_width), randint(0, window_height)))
+
 
 #general setup
 pygame.init()
@@ -11,31 +53,19 @@ pygame.display.set_caption("Space Shooter")
 running = True
 clock = pygame.time.Clock()
 
-#player stuff
-player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.Vector2(0, 0)
-player_speed = 300
+#group stuff
+all_sprites = pygame.sprite.Group()
 
-#star stuff
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
-stars = []
-
-#meteor stuff
-meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
-meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-meteor_direction = pygame.Vector2(1, -1)
-meteor_speed = 20
-
-
-#laser stuff
-laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
-
 for i in range(20):
-    x = randint(0, WINDOW_WIDTH)
-    y = randint(0, WINDOW_HEIGHT)
-    stars.append(pygame.Vector2(x, y))
+    Star(all_sprites, WINDOW_WIDTH, WINDOW_HEIGHT, star_surf)
+
+player = Player(all_sprites)
+
+
+# custom events -> meteor event
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 500)
 
 while running:
     #event loop
@@ -43,42 +73,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == meteor_event:
+            print('meteorrrrr')
 
     #i'm gonna put game logic here
 
-    #this is gonna be where i get input
-    keys = pygame.key.get_pressed()
-    player_direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-    player_direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
-    #normalize speed up and down
-    player_direction = player_direction.normalize() if player_direction else player_direction
-
-    if pygame.key.get_just_pressed()[pygame.K_SPACE]:
-        print("fire laser")
-
-
-    player_rect.center += player_direction * player_speed * dt
-
-    #meteor movement logic
-    meteor_rect.center += meteor_direction * meteor_speed
-    if meteor_rect.right >= WINDOW_WIDTH or meteor_rect.left <= 0:
-        meteor_direction.x *= -1
-    elif meteor_rect.top <= 0 or meteor_rect.bottom >= WINDOW_HEIGHT:
-        meteor_direction.y *= -1
-
+    all_sprites.update(dt)
 
     #draw the game
     display_surface.fill('darkgray')
-    for star in stars:
-        display_surface.blit(star_surf, star)
-    display_surface.blit(meteor_surf, meteor_rect)
-    display_surface.blit(laser_surf, laser_rect)
-    display_surface.blit(player_surf, player_rect)
-
+    all_sprites.draw(display_surface)
     pygame.display.update()
 
 
 
 pygame.quit()
 
-# left off at 1 hr 54 min 28 seconds
+# left off at 2 hr 23 min 18 seconds
